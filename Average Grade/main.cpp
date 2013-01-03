6c2b57950e3b1fa4742c478883981b11b1d4a995
kehlin swain <kehlinswain@kehlins-MacBook-Pro.local>
2013-01-02 19:24:47 -0600

    Initial Commit

---
+++ Average Grade/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Average Grade
+//
+//  Created by kehlin swain on 1/2/13.
+//  Copyright (c) 2013 kehlin swain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
